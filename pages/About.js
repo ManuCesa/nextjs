c79b538856f0c71df16b8071121dbb0dd4ca1d53
ManuCesa <52128651+ManuCesa@users.noreply.github.com>
2023-04-24 18:37:18 +0200

    1 binaries uploaded

--- pages/About.js
+++ pages/About.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 function About() {
   return (
@@ -12,20 +12,16 @@ function About() {
       </Head>
       <div className="flex w-full justify-start items-center py-14 lg:py-0 ">
         <p className="lg:w-[60%] w-full bg-[#F9FAFB]/10 lg:text-xl text-base text-black">
-          Seal Squad is a unique collection of 3,333 seals that will take you on
-          an exciting journey filled with adventures and rewards.
-          Well aim to push the boundaries of Web3 while building a community of like-minded
-          individuals who share a passion for the space.
-          With over 75 traits, each seal in our collection is unique! Once you own a seal, you become
-          a part of our community and gain access to exclusive benefits such
-          networking, alpha and giveaways! Our seals arent just cute PFPs; they
-          are essential in your ability to complete weekly missions! Use 1, or
-          form a squad (3) to tackle the rarest of missions and give your squad
-          a higher chance of success! Our weekly missions will test your skills
-          and strategic thinking, and offer you a chance to win amazing prizes.
-          Join us on this exciting journey and become a part of our growing
-          community. Collect your favorite seals, form a squad, and get ready to
-          test your skills!
+          Introducing Seal Squad, a collection of 3,333 unique, pixelated seals
+          invading Web3! These adorable creatures are anything but harmless...
+          Don't let their cuteness fool you.
+          <br></br>
+          <br></br>
+          To join the action, you'll need to establish your #squad of seals,
+          which you'll need to complete missions for rewards. Missions will vary
+          in difficulty, and while some can be completed solo, others will
+          require an elite team of three. The stakes are high, but the rewards
+          are even higher. Choose your squad carefully.
         </p>
       </div>
     </main>
