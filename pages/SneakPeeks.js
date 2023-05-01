8685a60076736bfa18c512a1b2c1ff01bc33670a
ManuCesa <52128651+ManuCesa@users.noreply.github.com>
2023-05-01 15:26:57 +0200

    1 binaries uploaded

--- pages/SneakPeeks.js
+++ pages/SneakPeeks.js
@@ -1,24 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import SQ1F from '/public/assets/SQ1F.png';
-import SQ2F from '/public/assets/SQ2F.png';
-import SQ3F from '/public/assets/SQ3F.png';
-import SQ4F from '/public/assets/SQ4F.png';
-import SQ5F from '/public/assets/SQ5F.png';
-import SQ6F from '/public/assets/SQ6F.png';
-import SQ7F from '/public/assets/SQ7F.png';
-import SQ8F from '/public/assets/SQ8F.png';
-import SQ9F from '/public/assets/SQ9F.png';
-import SQ10F from '/public/assets/SQ10F.png';
-import SQ11F from '/public/assets/SQ11F.png';
-import SQ12F from '/public/assets/SQ12F.png';
-import SQ13F from '/public/assets/SQ13F.png';
-import SQ14F from '/public/assets/SQ14F.png';
+import SQ1 from '/public/assets/SQ1.png';
+import SQ2 from '/public/assets/SQ2.png';
+import SQ3 from '/public/assets/SQ3.png';
+import SQ4 from '/public/assets/SQ4.png';
+import SQ5 from '/public/assets/SQ5.png';
+import SQ6 from '/public/assets/SQ6.png';
+import SQ7 from '/public/assets/SQ7.png';
+import SQ8 from '/public/assets/SQ8.png';
+import SQ9 from '/public/assets/SQ9.png';
+import SQ10 from '/public/assets/SQ10.png';
+import SQ11 from '/public/assets/SQ11.png';
+import SQ12 from '/public/assets/SQ12.png';
+import SQ13 from '/public/assets/SQ13.png';
+import SQ14 from '/public/assets/SQ14.png';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards, Navigation } from 'swiper';
 import 'swiper/css';
 import React, { useRef, useState } from 'react';
 import 'swiper/css/navigation';
+import 'swiper/css/effect-cards';
 import { Autoplay } from 'swiper';
 function SneakPeeks() {
   return (
@@ -29,45 +31,58 @@ function SneakPeeks() {
         <title>Seal Squad - SneakPeeks</title>
         <meta name="description" content="Seal Squad" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="./global.css" />
       </Head>
-      <div className="w-3/4 flex justify-center items-center">
+
+      <div className="BoxSwiper">
         <Swiper
           navigation={true}
-          slidesPerView={3}
           modules={[Autoplay, Navigation, EffectCards]}
           effect={'cards'}
           grabCursor={true}
           centeredSlides={true}
         >
           <SwiperSlide>
-            <div>
-              <Image src={SQ1F} alt="SQ1F" />
-            </div>
+            <Image src={SQ1} alt="SQ1F" />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <Image src={SQ2F} alt="SQ2F" />
-            </div>
+            <Image src={SQ2} alt="SQ2F" />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <Image src={SQ3F} alt="SQ3F" />
-            </div>
+            <Image src={SQ3} alt="SQ3F" />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <Image src={SQ4F} alt="SQ4F" />
-            </div>
+            <Image src={SQ4} alt="SQ4F" />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <Image src={SQ5F} alt="SQ5F" />
-            </div>
+            <Image src={SQ5} alt="SQ5F" />
           </SwiperSlide>
           <SwiperSlide>
-            <div>
-              <Image src={SQ6F} alt="SQ6F" />
-            </div>
+            <Image src={SQ6} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ7} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ8} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ9} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ10} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ11} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ12} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ13} alt="SQ6F" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src={SQ14} alt="SQ6F" />
           </SwiperSlide>
         </Swiper>
       </div>
