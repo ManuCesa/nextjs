951b92026c7d30e84a453aec065f0587bf89da08
ManuCesa <52128651+ManuCesa@users.noreply.github.com>
2023-05-01 18:52:57 +0200

    1 file modified

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '/public/assets/seal-mlogo.png';
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 function Header() {
   const [isOpen, setisOpen] = useState(false);
